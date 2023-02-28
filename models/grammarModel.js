7f29b2940b14fba40671ea847dee2980903b5155
Le Phu Quoc <tygovip119@gmail.com>
2023-03-01 02:29:46 +0700

    GrammarModel: set up virtual properties

--- models/grammarModel.js
+++ models/grammarModel.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose');
 
-const grammarSchema = new mongoose.Schema({
-  grammar: {
-    type: String,
-    required: [true, 'A grammar must have a valid name'],
-    unique: [true, 'A grammar must be unique'],
+const grammarSchema = new mongoose.Schema(
+  {
+    grammar: {
+      type: String,
+      required: [true, 'A grammar must have a valid name'],
+      unique: [true, 'A grammar must be unique'],
+    },
+    content: {
+      type: String,
+      required: [true, 'A grammar must have a valid content'],
+    },
   },
-  content: {
-    type: String,
-    required: [true, 'A grammar must have a valid content'],
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+grammarSchema.virtual('grammarLength').get(function () {
+  return this.grammar.length;
 });
 
 const Grammar = mongoose.model('Grammar', grammarSchema);
