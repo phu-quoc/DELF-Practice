03dc9b01c04665a204d1b3d0889c36fc3fee43a1
Le Phu Quoc <tygovip119@gmail.com>
2023-03-01 05:04:48 +0700

    GrammarModel: middleware

--- models/grammarModel.js
+++ models/grammarModel.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const grammarSchema = new mongoose.Schema(
   {
     grammar: {
       type: String,
       required: [true, 'A grammar must have a valid name'],
-      unique: [true, 'A grammar must be unique'],
+      unique: true,
     },
+    slug: String,
     content: {
       type: String,
       required: [true, 'A grammar must have a valid content'],
@@ -26,6 +28,35 @@ grammarSchema.virtual('grammarLength').get(function () {
   return this.grammar.length;
 });
 
+//DOCUMENT MIDDLEWARE
+grammarSchema.pre('save', function (next) {
+  this.slug = slugify(this.grammar, {
+    lower: true,
+  });
+  next();
+});
+
+//QUERY MIDDLEWARE
+grammarSchema.pre(/^find/, function (next) {
+  this.find({
+    grammar: {
+      $ne: 'Past simple tense',
+    },
+  });
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+grammarSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: {
+      grammar: {
+        $ne: 'Present continuous tense',
+      },
+    },
+  });
+  next();
+});
 const Grammar = mongoose.model('Grammar', grammarSchema);
 
 module.exports = Grammar;
