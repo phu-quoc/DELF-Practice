3c38a705b07ae140b710649cc43dad4f97c0d0eb
Le Phu Quoc <tygovip119@gmail.com>
2023-03-03 01:44:41 +0700

    Error Handling Middleware

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
 const grammarRouter = require('./routes/grammarRoutes');
 
 const app = express();
 
-// 1. MIDDLEWAREs
+// MIDDLEWAREs
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 2. ROUTE HANDLERS
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-// 3. ROUTES
+// ROUTES
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/grammars', grammarRouter);
+app.use('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
 
 module.exports = app;
