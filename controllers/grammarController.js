aae93e41a4971b37e077364d56779e69ea44804d
Le Phu Quoc <tygovip119@gmail.com>
2023-03-03 17:07:14 +0700

    Catch Error Async

--- controllers/grammarController.js
+++ controllers/grammarController.js
@@ -1,5 +1,6 @@
 const Grammar = require('../models/grammarModel');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 
 exports.aliasTopGrammars = (req, res, next) => {
   req.query.limit = '5';
@@ -8,189 +9,140 @@ exports.aliasTopGrammars = (req, res, next) => {
   next();
 };
 
-exports.getAllGrammars = async (req, res) => {
-  try {
-    const features = new APIFeatures(Grammar.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const grammars = await features.query;
-    res.status(200).json({
-      status: 'success',
-      results: grammars.length,
-      data: {
-        grammars,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.getAllGrammars = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(Grammar.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const grammars = await features.query;
+  res.status(200).json({
+    status: 'success',
+    results: grammars.length,
+    data: {
+      grammars,
+    },
+  });
+});
 
-exports.getGrammar = async (req, res) => {
-  try {
-    const grammar = await Grammar.findById(req.params.id);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        grammar,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.getGrammar = catchAsync(async (req, res, next) => {
+  const grammar = await Grammar.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      grammar,
+    },
+  });
+});
 
-exports.createGrammar = async (req, res) => {
-  try {
-    const grammar = await Grammar.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        grammar: grammar,
-      },
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.createGrammar = catchAsync(async (req, res, next) => {
+  const grammar = await Grammar.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      grammar: grammar,
+    },
+  });
+});
 
-exports.updateGrammar = async (req, res) => {
-  try {
-    const grammar = await Grammar.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        grammar: grammar,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.updateGrammar = catchAsync(async (req, res, next) => {
+  const grammar = await Grammar.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      grammar: grammar,
+    },
+  });
+});
 
-exports.deleteGrammar = async (req, res) => {
-  try {
-    await Grammar.findByIdAndDelete(req.params.id);
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+exports.deleteGrammar = catchAsync(async (req, res, next) => {
+  await Grammar.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
-exports.getGrammarStats = async (req, res) => {
-  try {
-    const stats = await Grammar.aggregate([
-      {
-        $match: {
-          grammar: {
-            $gte: 0,
-          },
-        },
-        $group: {
-          _id: {
-            $toUpper: '$grammar',
-          },
-          numGrammars: {
-            $sum: 1,
-          },
-        },
-        $sort: {
-          grammar: 1,
+exports.getGrammarStats = catchAsync(async (req, res, next) => {
+  const stats = await Grammar.aggregate([
+    {
+      $match: {
+        grammar: {
+          $gte: 0,
         },
       },
-    ]);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        stats,
+      $group: {
+        _id: {
+          $toUpper: '$grammar',
+        },
+        numGrammars: {
+          $sum: 1,
+        },
       },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+      $sort: {
+        grammar: 1,
+      },
+    },
+  ]);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      stats,
+    },
+  });
+});
 
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const year = req.params.year * 1;
-    const plan = Grammar.aggregate([
-      {
-        $unwind: '$startDates',
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1;
+  const plan = Grammar.aggregate([
+    {
+      $unwind: '$startDates',
+    },
+    {
+      $match: {
+        $gte: new Date(`${year}-01-01`),
+        $lte: new Date(`${year}-12-31`),
       },
-      {
-        $match: {
-          $gte: new Date(`${year}-01-01`),
-          $lte: new Date(`${year}-12-31`),
+    },
+    {
+      $group: {
+        _id: {
+          $month: '$startDates',
+        },
+        numGrammarStarts: {
+          $sum: 1,
+        },
+        grammar: {
+          $push: '$name',
         },
       },
-      {
-        $group: {
-          _id: {
-            $month: '$startDates',
-          },
-          numGrammarStarts: {
-            $sum: 1,
-          },
-          grammar: {
-            $push: '$name',
-          },
-        },
+    },
+    {
+      $addFields: {
+        month: '$_id',
       },
-      {
-        $addFields: {
-          month: '$_id',
-        },
+    },
+    {
+      $project: {
+        _id: 0,
       },
-      {
-        $project: {
-          _id: 0,
-        },
+    },
+    {
+      $sort: {
+        numGrammarStarts: -1,
       },
-      {
-        $sort: {
-          numGrammarStarts: -1,
-        },
-      },
-      {
-        $limit: 12,
-      },
-    ]);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        plan,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: 'fail',
-      message: err,
-    });
-  }
-};
+    },
+    {
+      $limit: 12,
+    },
+  ]);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      plan,
+    },
+  });
+});
