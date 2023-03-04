2b3430f411a959e070e2e82f75e307cd86123e57
Le Phu Quoc <tygovip119@gmail.com>
2023-03-05 02:42:17 +0700

    Catch Error Not Found ID

--- routes/grammarRoutes.js
+++ routes/grammarRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const grammarController = require('../controllers/grammarController');
-const userController = require('../controllers/userController');
 
 const router = express.Router();
 
