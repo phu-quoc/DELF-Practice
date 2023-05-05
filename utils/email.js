8cbd93ae96d00fc9e80e7f2ff75eeaf7d430317f
Le Phu Quoc <tygovip119@gmail.com>
2023-05-06 02:34:33 +0700

    Send gmail forgot password

--- utils/email.js
+++ utils/email.js
@@ -1,12 +1,24 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
 const sendEmail = async options => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: 'gmail',
     auth: {
+      type: 'OAuth2',
       user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REDIRECT_URI,
+      accessToken: process.env.GOOGLE_ACCESS_TOKEN,
     },
   });
 
