8d09729e12e8a8a8b91c580966bc07db8951b122
Le Phu Quoc <tygovip119@gmail.com>
2023-05-17 13:52:44 +0700

    Fix calculating score

--- models/answerModel.js
+++ models/answerModel.js
@@ -40,7 +40,7 @@ answerSchema.statics.calcTotalScore = async function (resultId) {
 answerSchema.pre('save', async function (next) {
   const question = await Question.findById(this.question);
   const correctAnswer = question.options.filter(
-    option => option._id === this.answer && option.isCorrect === true
+    option => option._id == this.answer && option.isCorrect === true
   );
   console.log('correctAnswer', correctAnswer.length);
   console.log('question', question.options);
