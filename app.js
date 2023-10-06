e68bfc43e35787806d6e16b984ad9b37e74e41c7
Le Phu Quoc <tygovip119@gmail.com>
2023-10-06 14:42:55 +0700

    add dependencies

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const vocabularyRouter = require('./routes/vocabularyRoutes');
 const answerRouter = require('./routes/answerRoutes');
 const exerciseRouter = require('./routes/exerciseRoutes');
 const favoriteRouter = require('./routes/favoriteRoutes');
+const paymentRouter = require('./routes/paymentRoutes');
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use('/api/v1/vocabularies', vocabularyRouter);
 app.use('/api/v1/answers', answerRouter);
 app.use('/api/v1/exercises', exerciseRouter);
 app.use('/api/v1/favorites', favoriteRouter);
+app.use('/api/v1/payment', paymentRouter);
 app.use('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
